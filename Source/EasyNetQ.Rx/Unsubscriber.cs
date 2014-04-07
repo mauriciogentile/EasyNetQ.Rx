94822646562bc520b5be5f173dfe99f3afbc0e23
Mauricio Gentile <gentilemauricio@gmail.com>
2014-04-07 11:02:58 -0400

    Separating Unsuscriber class into new file

---
+++ Source/EasyNetQ.Rx/Unsubscriber.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+namespace EasyNetQ.Rx
+{
+    class Unsubscriber<T> : IDisposable
+    {
+        private readonly List<IObserver<T>> _observers;
+        private readonly IObserver<T> _observer;
+
+        public Unsubscriber(List<IObserver<T>> observers, IObserver<T> observer)
+        {
+            _observers = observers;
+            _observer = observer;
+        }
+
+        public void Dispose()
+        {
+            if (_observer != null && _observers.Contains(_observer))
+                _observers.Remove(_observer);
+        }
+    }
+}
