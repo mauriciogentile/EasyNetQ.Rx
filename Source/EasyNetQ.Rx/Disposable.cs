ad2140edd2d3cf2c9f68fef3980cab3e70bd5164
Mauricio Gentile <gentilemauricio@gmail.com>
2014-04-15 15:31:00 -0400

    adding missing file

---
+++ Source/EasyNetQ.Rx/Disposable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EasyNetQ.Rx
+{
+    public abstract class Disposable : IDisposable
+    {
+        bool _disposed;
+
+        protected Action OnDispose;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing && OnDispose != null)
+            {
+                OnDispose();
+            }
+
+            _disposed = true;
+        }
+
+        ~Disposable()
+        {
+            Dispose(false);
+        }
+    }
+}
